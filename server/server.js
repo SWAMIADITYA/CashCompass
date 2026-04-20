b8dc25ba28d50b8e1121617dc8f0a0367d67f56c
SWAMIADITYA <adityaswami098@gmail.com>
2026-04-21 03:36:36 +0530

    fix cors

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const dashboardRoutes = require('./src/routes/dashboardRoutes');
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,7 +1,5 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config();
 
-const MYSQL_URL = `mysql://root:omWWFexYrCQYlyEKoAWdTYJaHzlcyZmw@mysql.railway.internal:3306/railway`
 const pool = mysql.createPool(process.env.MYSQL_URL);
 
 module.exports = pool;
