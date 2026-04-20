15ec8b97876db6dfdcb9de58993dfe2b4fc036c2
SWAMIADITYA <adityaswami098@gmail.com>
2026-04-21 04:14:31 +0530

    fix api routes

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,13 +1,13 @@
 import api from './axios';
 
 export const registerUser = (data) => {
-  return api.post('/auth/register', data);
+  return api.post('/api/auth/register', data);
 };
 
 export const loginUser = (data) => {
-  return api.post('/auth/login', data);
+  return api.post('/api/auth/login', data);
 };
 
 export const getCurrentUser = () => {
-  return api.get('/auth/me');
+  return api.get('/api/auth/me');
 };
--- client/src/api/budgetApi.js
+++ client/src/api/budgetApi.js
@@ -1,26 +1,26 @@
 import api from './axios';
 
 export const getAllBudgets = async () => {
-  const response = await api.get('/budgets');
+  const response = await api.get('/api/budgets');
   return response.data;
 };
 
 export const getBudgetById = async (id) => {
-  const response = await api.get(`/budgets/${id}`);
+  const response = await api.get(`/api/budgets/${id}`);
   return response.data;
 };
 
 export const createBudget = async (budgetData) => {
-  const response = await api.post('/budgets', budgetData);
+  const response = await api.post('/api/budgets', budgetData);
   return response.data;
 };
 
 export const updateBudget = async (id, budgetData) => {
-  const response = await api.put(`/budgets/${id}`, budgetData);
+  const response = await api.put(`/api/budgets/${id}`, budgetData);
   return response.data;
 };
 
 export const deleteBudget = async (id) => {
-  const response = await api.delete(`/budgets/${id}`);
+  const response = await api.delete(`/api/budgets/${id}`);
   return response.data;
 };
