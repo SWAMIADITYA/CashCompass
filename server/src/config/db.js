c12666cc25d7ab7e444cb07b9e5357a27ba0985a
SWAMIADITYA <adityaswami098@gmail.com>
2026-04-21 02:02:04 +0530

    update db.js and added railway.json file in the root

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2/promise');
 require('dotenv').config();
 
-const urlDB = `mysql://root:omWWFexYrCQYlyEKoAWdTYJaHzlcyZmw@mysql.railway.internal:3306/railway`
-const pool = mysql.createPool(urlDB);
+const MYSQL_URL = `mysql://root:omWWFexYrCQYlyEKoAWdTYJaHzlcyZmw@mysql.railway.internal:3306/railway`
+const pool = mysql.createPool(process.env.MYSQL_URL);
 
 module.exports = pool;
 
