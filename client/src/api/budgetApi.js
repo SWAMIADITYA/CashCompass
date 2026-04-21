daaf4dcc961e0cf31251205a677be3ab16441257
SWAMIADITYA <adityaswami098@gmail.com>
2026-04-21 17:45:06 +0530

    budgetApi modified

--- client/src/api/budgetApi.js
+++ client/src/api/budgetApi.js
@@ -1,26 +1,26 @@
 import api from './axios';
 
 export const getAllBudgets = async () => {
-  const response = await api.get('/api/budgets');
+  const response = await api.get('/budgets');
   return response.data;
 };
 
 export const getBudgetById = async (id) => {
-  const response = await api.get(`/api/budgets/${id}`);
+  const response = await api.get(`/budgets/${id}`);
   return response.data;
 };
 
 export const createBudget = async (budgetData) => {
-  const response = await api.post('/api/budgets', budgetData);
+  const response = await api.post('/budgets', budgetData);
   return response.data;
 };
 
 export const updateBudget = async (id, budgetData) => {
-  const response = await api.put(`/api/budgets/${id}`, budgetData);
+  const response = await api.put(`/budgets/${id}`, budgetData);
   return response.data;
 };
 
 export const deleteBudget = async (id) => {
-  const response = await api.delete(`/api/budgets/${id}`);
+  const response = await api.delete(`/budgets/${id}`);
   return response.data;
 };
